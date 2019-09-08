0eddea1c2c181b605ba2fd36ac9ba7e95499a4f3
luffy <luffybryant@gmail.com>
2019-09-09 01:20:19 +0800

    update

--- assets/plugin.js
+++ assets/plugin.js
@@ -1,19 +1,26 @@
-require('gitalk/dist/gitalk.css')
-const gitbook = require('gitbook')
-const Gitalk = require('gitalk')
+require(['gitbook', 'gittalk'], function(gitbook, Gitalk) {
+  gitbook.events.bind('start', function() {
+    var link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = 'https://unpkg.com/gitalk/dist/gitalk.css'
+    document.getElementsByTagName('head')[0].appendChild(link)
+    var script = document.createElement('script')
+    script.src = 'https://unpkg.com/gitalk/dist/gitalk.min.js'
+    document.body.appendChild(script)
+    var div = document.createElement('div')
+    div.setAttribute('id', 'gitalk-container')
+    document.body.appendChild(div)
+  })
 
-gitbook.events.bind('page.before', function(page) {
-  var str =
-    '<link rel="stylesheet" href="https://unpkg.com/gitalk/dist/gitalk.css">' +
-    '<script src="https://unpkg.com/gitalk/dist/gitalk.min.js"></script>' +
-    '<div id="gitalk-container"></div>'
-  page.content += str
-  return page
-})
+  gitbook.events.bind('page.change', function() {
+    let config = gitbook.state.config.pluginsConfig
+    var _defaults = {
+      id: location.href
+    }
+    var options = Object.assign({}, _defaults, config.gittalk)
+    console.log(window)
+    const gitalk = new Gitalk(options)
 
-gitbook.events.bind('page.change', function() {
-  var options = object.assign({}, this.options.pluginsConfig['gittalk'])
-  const gitalk = new Gitalk(options)
-
-  gitalk.render('gitalk-container')
+    gitalk.render('gitalk-container')
+  })
 })
