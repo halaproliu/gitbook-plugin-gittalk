79253dfe32a4f78dc9c8f56b7535365840fafdd8
liuwenjian <liuwenjian091@pingan.com.cn>
2019-09-09 15:26:46 +0800

    update

--- assets/plugin.js
+++ assets/plugin.js
@@ -1,25 +1,11 @@
-require(['gitbook', 'gittalk'], function(gitbook, Gitalk) {
-  gitbook.events.bind('start', function() {
-    var link = document.createElement('link')
-    link.rel = 'stylesheet'
-    link.href = 'https://unpkg.com/gitalk/dist/gitalk.css'
-    document.getElementsByTagName('head')[0].appendChild(link)
-    var script = document.createElement('script')
-    script.src = 'https://unpkg.com/gitalk/dist/gitalk.min.js'
-    document.body.appendChild(script)
-    var div = document.createElement('div')
-    div.setAttribute('id', 'gitalk-container')
-    document.body.appendChild(div)
-  })
-
+require(['gitbook'], function(gitbook) {
   gitbook.events.bind('page.change', function() {
-    let config = gitbook.state.config.pluginsConfig
+    var config = gitbook.state.config.pluginsConfig
     var _defaults = {
-      id: location.href
+      id: window.md5(location.href)
     }
     var options = Object.assign({}, _defaults, config.gittalk)
-    console.log(window)
-    const gitalk = new Gitalk(options)
+    var gitalk = new window.Gitalk(options)
 
     gitalk.render('gitalk-container')
   })
